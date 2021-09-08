df9f64fe3e116ad4770be675250cd31c24a62064
Adrian <eadepto@hotmail.com>
2021-09-08 17:10:39 -0500

    Hasta transacciones

--- 02-firebase/app/src/main/java/com/example/firebaseuno/DRestaurante.kt
+++ 02-firebase/app/src/main/java/com/example/firebaseuno/DRestaurante.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.Query
 
 class DRestaurante : AppCompatActivity() {
+    var query: Query? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drestaurante)
@@ -21,7 +25,7 @@ class DRestaurante : AppCompatActivity() {
         val botonDatosPrueba = findViewById<Button>(R.id.btn_datos_prueba)
         botonDatosPrueba
             .setOnClickListener {
-                crearDatos()
+                transaccion()
             }
         val botonConsultar = findViewById<Button>(R.id.btn_consultar)
         botonConsultar
@@ -29,7 +33,137 @@ class DRestaurante : AppCompatActivity() {
                 consultar()
             }
     }
-    fun crearDatos(){
+
+    fun transaccion() {
+        val db = Firebase.firestore
+        val referenciaCities = db.collection("cities").document("SF")
+        ///////
+        db.runTransaction {
+                transaction ->
+            val documentoActual = transaction.get(referenciaCities)
+            val poblacion = documentoActual.getDouble("population")
+            if(poblacion != null){
+                val nuevaPoblacion = poblacion + 1
+                transaction.update(referenciaCities, "population", nuevaPoblacion)
+            }
+        }
+            .addOnSuccessListener { Log.i("transaccion", "Transaccion completada") }
+            .addOnFailureListener { Log.i("transaccion", "ERROR") }
+
+    }
+
+
+
+    fun consultar() {
+        /*
+           < less than
+           <= less than or equal to
+           == equal to
+           > greater than
+           >= greater than or equal to
+           != not equal to
+           array-contains
+           array-contains-any
+           in
+           not-in
+       * */
+        /*
+        citiesRef
+            .document("BJ") // ID
+            .get()
+            .addOnSuccessListener {
+                Log.i("consultas", "${it.data}")
+            }
+            .addOnFailureListener {  }
+
+        citiesRef
+            .whereEqualTo("country", "China")
+            .get()
+            .addOnSuccessListener {
+                Log.i("consultas", "${it.documents}") // arreglo con metadatos
+                for (ciudad in it){
+                    Log.i("consultas", "${ciudad.data}")
+                    Log.i("consultas", "${ciudad.id}")
+                }
+            }
+            .addOnFailureListener {  }
+
+
+        // Buscar por dos o mas elementos campo '==' 'array-contains'
+        citiesRef
+            .whereEqualTo("capital", false)
+            .whereArrayContainsAny("regions", arrayListOf("socal", "norcal"))
+            .get()
+            .addOnSuccessListener {
+                for (ciudad in it){
+                    Log.i("consultas", "== array-contains ${ciudad.data}")
+                }
+            }
+
+        // Buscar por dos o mas elementos campo '==' '>='
+        citiesRef
+            .whereEqualTo("capital", true)
+            .whereGreaterThanOrEqualTo("population", 1000000)
+            .get()
+            .addOnSuccessListener {
+                for (ciudad in it){
+                    Log.i("consultas", "== array-contains ${ciudad.data}")
+                }
+            }
+
+        // Buscar por dos o mas elementos campo '==' '<='
+
+        citiesRef
+            .whereEqualTo("capital", false)
+            .whereLessThanOrEqualTo("population", 4000000)
+            .orderBy("population", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener {
+                for (ciudad in it){
+                    Log.i("consultas", "${ciudad.data}")
+                }
+            }
+
+        */
+
+
+        val db = Firebase.firestore
+        val refCities = db.collection("cities")
+            .orderBy("population")
+            .limit(2)
+        var tarea: Task<QuerySnapshot>? = null
+        if (query == null) {
+            tarea = refCities.get()
+        } else {
+            tarea = query!!.get()
+        }
+        if(tarea != null){
+            tarea
+                .addOnSuccessListener { documentSnapshots ->
+                    guardarQuery(documentSnapshots, refCities)
+                    for (ciudad in documentSnapshots) {
+                        Log.i("consultas", "${ciudad.data}")
+                    }
+                }
+                .addOnFailureListener {
+                    Log.i("consultas", "ERROR: ${it}")
+                }
+        }
+
+
+    }
+
+    fun guardarQuery(documentSnapshots: QuerySnapshot, refCities: Query) {
+        if (documentSnapshots.size() > 0) {
+            val ultimoDocumento = documentSnapshots.documents[documentSnapshots.size() - 1]
+            query = refCities
+                .startAfter(ultimoDocumento)
+        } else {
+
+        }
+    }
+
+    fun crearDatos() {
         val db = Firebase.firestore
         val cities = db.collection("cities")
 
@@ -83,73 +217,8 @@ class DRestaurante : AppCompatActivity() {
         )
         cities.document("BJ").set(data5)
     }
-    fun consultar(){
 
-        /*
-           < less than
-           <= less than or equal to
-           == equal to
-           > greater than
-           >= greater than or equal to
-           != not equal to
-           array-contains
-           array-contains-any
-           in
-           not-in
-       * */
-        val db = Firebase.firestore
-        val citiesRef = db.collection("cities")
-
-        /*
-        citiesRef
-            .document("BJ") // ID
-            .get()
-            .addOnSuccessListener {
-                Log.i("consultas", "${it.data}")
-            }
-            .addOnFailureListener {  }
-
-        citiesRef
-            .whereEqualTo("country", "China")
-            .get()
-            .addOnSuccessListener {
-                Log.i("consultas", "${it.documents}") // arreglo con metadatos
-                for (ciudad in it){
-                    Log.i("consultas", "${ciudad.data}")
-                    Log.i("consultas", "${ciudad.id}")
-                }
-            }
-            .addOnFailureListener {  }
-
-
-        // Buscar por dos o mas elementos campo '==' 'array-contains'
-        citiesRef
-            .whereEqualTo("capital", false)
-            .whereArrayContainsAny("regions", arrayListOf("socal", "norcal"))
-            .get()
-            .addOnSuccessListener {
-                for (ciudad in it){
-                    Log.i("consultas", "== array-contains ${ciudad.data}")
-                }
-            }
-
-        */
-
-        // Buscar por dos o mas elementos campo '==' '>='
-        citiesRef
-            .whereEqualTo("capital", true)
-            .whereGreaterThanOrEqualTo("population", 1000000)
-            .get()
-            .addOnSuccessListener {
-                for (ciudad in it){
-                    Log.i("consultas", "== array-contains ${ciudad.data}")
-                }
-            }
-
-
-    }
-
-    fun crearRestaurante(){
+    fun crearRestaurante() {
         val editTextNombre = findViewById<EditText>(R.id.et_nombre_restaurante)
         val nuevoRestaurante = hashMapOf<String, Any>(
             "nombre" to editTextNombre.text.toString()
