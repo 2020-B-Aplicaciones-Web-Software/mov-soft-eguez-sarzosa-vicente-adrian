dccf6b31ec7f59574cc8835a6358676a4b0bdd2f
Adrian <eadepto@hotmail.com>
2021-09-15 18:02:31 -0500

    final

--- 02-firebase/app/src/main/java/com/example/firebaseuno/FMapsActivity.kt
+++ 02-firebase/app/src/main/java/com/example/firebaseuno/FMapsActivity.kt
@@ -3,6 +3,8 @@ package com.example.firebaseuno
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -10,6 +12,8 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
 
 class FMapsActivity : AppCompatActivity() {
     private lateinit var mapa: GoogleMap
@@ -21,10 +25,19 @@ class FMapsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_fmaps)
         solicitarPermisos()
 
+        val botonCarolina = findViewById<Button>(R.id.btn_ir_carolina)
+        botonCarolina
+            .setOnClickListener {
+                val carolina = LatLng(-0.18288452555103193, -78.48449971346241)
+                val zoom = 17f
+                moverCamaraConZoom(carolina, zoom)
+            }
+
+
         val fragmentoMapa = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         fragmentoMapa.getMapAsync { googleMap ->
-            if(googleMap != null){
+            if (googleMap != null) {
                 mapa = googleMap
                 establecerConfiguracionMapa()
                 val quicentro = LatLng(-0.176125, -78.480208)
@@ -32,17 +45,75 @@ class FMapsActivity : AppCompatActivity() {
                 val zoom = 17f
                 anadirMarcador(quicentro, titulo)
                 moverCamaraConZoom(quicentro, zoom)
+
+                // LINEA
+                val poliLineaUno = googleMap
+                    .addPolyline(
+                        PolylineOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.175327, -78.488211),
+                                LatLng(-0.17158712906364446, -78.48483202848371),
+                                LatLng(-0.17680657057716478, -78.47801941206454)
+                            )
+                    )
+                poliLineaUno.tag = "linea-1" // <- ID
+
+                // POLIGONO
+                val poligonoUno = googleMap
+                    .addPolygon(
+                        PolygonOptions()
+                            .clickable(true)
+                            .add(
+                                LatLng(-0.1771546902239471, -78.48344981495214),
+                                LatLng(-0.17968981486125768, -78.48269198043828),
+                                LatLng(-0.17710958124147777, -78.48142892291516)
+                            )
+                    )
+                poligonoUno.fillColor = -0xc771c4
+                poligonoUno.tag = "poligono-2" // <- ID
+                escucharListeners()
             }
         }
     }
-    fun anadirMarcador(latLng: LatLng, title: String){
+    fun escucharListeners() {
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("mapa", "setOnPolylineClickListener ${it}")
+        }
+
+        mapa
+            .setOnMarkerClickListener {
+                Log.i("mapa", "setOnMarkerClickListener ${it}")
+                return@setOnMarkerClickListener true
+            }
+        mapa
+            .setOnCameraMoveListener {
+                Log.i("mapa", "setOnCameraMoveListener")
+            }
+        mapa
+            .setOnCameraMoveStartedListener {
+                Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+            }
+
+        mapa
+            .setOnCameraIdleListener {
+                Log.i("mapa", "setOnCameraIdleListener")
+            }
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String) {
         mapa.addMarker(
             MarkerOptions()
                 .position(latLng)
                 .title(title)
         )
     }
-    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f){
+
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f) {
         mapa.moveCamera(
             CameraUpdateFactory
                 .newLatLngZoom(latLng, zoom)
@@ -50,17 +121,7 @@ class FMapsActivity : AppCompatActivity() {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-    fun solicitarPermisos(){
+    fun solicitarPermisos() {
         val contexto = this.applicationContext
         val permisosFineLocation = ContextCompat
             .checkSelfPermission(
@@ -68,9 +129,9 @@ class FMapsActivity : AppCompatActivity() {
                 android.Manifest.permission.ACCESS_FINE_LOCATION
             )
         val tienePermisos = permisosFineLocation == PackageManager.PERMISSION_GRANTED
-        if(tienePermisos){
+        if (tienePermisos) {
             permisos = true
-        }else{
+        } else {
             ActivityCompat.requestPermissions(
                 this,
                 arrayOf(  // Arreglo Permisos
@@ -83,60 +144,22 @@ class FMapsActivity : AppCompatActivity() {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    fun establecerConfiguracionMapa(){
+    fun establecerConfiguracionMapa() {
         val contexto = this.applicationContext
-        with(mapa){
-            val permisosFineLocation  = ContextCompat
+        with(mapa) {
+            val permisosFineLocation = ContextCompat
                 .checkSelfPermission(
                     contexto,
                     android.Manifest.permission.ACCESS_FINE_LOCATION
                 )
             val tienePermisos = permisosFineLocation == PackageManager.PERMISSION_GRANTED
-            if(tienePermisos){
+            if (tienePermisos) {
                 mapa.isMyLocationEnabled = true // no tenemos aun permisos
             }
             uiSettings.isZoomControlsEnabled = true
             uiSettings.isMyLocationButtonEnabled = true // no tenemos aun permisos
         }
     }
-
-
-
-
-
-
-
-
 
 
 }
